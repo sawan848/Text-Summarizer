9348ca48c6584110525bb5e5286a3467830af644
saone <ksawan802@gmail.com>
2025-01-03 20:39:19 +0530

    requirements & installtion done

--- template.py
+++ template.py
@@ -27,3 +27,19 @@ list_of_files =[
     "setup.py",
     "research/trials.ipynb"
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating diectory:{filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+            
+    else:
+        logging.info(f"{filename} is already exists")
